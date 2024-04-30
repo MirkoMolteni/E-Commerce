dbb49b2eaace5dbaa91801ffefce3427a7031ecb
MirkoMolteni <mirko.molteni.sc@gmail.com>
2024-04-30 09:57:12 +0200

    Fixato addProdotto

--- addProdotto.php
+++ addProdotto.php
@@ -5,43 +5,43 @@ include 'config.php';
 $idUser = $_SESSION['id'];
 $idProdotto = $_POST['idProdotto'];
 $quantita = $_POST['quantita'];
-$sql = "SELECT ID FROM ".$prefix."carrello WHERE idUtente = $idUser";
+$sql = "SELECT ID FROM " . $prefix . "carrello WHERE idUtente = $idUser";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $sql = "INSERT INTO carrello (idUtente) VALUES ($idUser)";
+    $conn->query($sql);
+    $idCarrello = $conn->insert_id;
+} else {
+    $row = $result->fetch_assoc();
+    $idCarrello = $row['ID'];
+
+}
+$sql = "SELECT * FROM " . $prefix . "prodotto WHERE ID = $idProdotto";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $idCarrello = $row['ID'];
-    $sql = "SELECT * FROM ".$prefix."prodotto WHERE ID = $idProdotto";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $quantitaDisponibile = $row['Quantita'];
-        if ($quantitaDisponibile >= $quantita) {
-            $sql = "SELECT * FROM ".$prefix."aggiunta WHERE idProdotto = $idProdotto AND idCarrello = $idCarrello";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $quantitaCarrello = $row['Quantita'];
-                $quantitaCarrello += $quantita;
-                $sql = "UPDATE ".$prefix."aggiunta SET Quantita = $quantitaCarrello WHERE idProdotto = $idProdotto AND idCarrello = $idCarrello";
-                $conn->query($sql);
-            } else {
-                $sql = "INSERT INTO ".$prefix."aggiunta (idProdotto, idCarrello, quantita) VALUES ($idProdotto, $idCarrello, $quantita)";
-                $conn->query($sql);
-            }
-            $quantitaDisponibile -= $quantita;
-            $sql = "UPDATE ".$prefix."prodotto SET quantita = $quantitaDisponibile WHERE ID = $idProdotto";
+    $quantitaDisponibile = $row['Quantita'];
+    if ($quantitaDisponibile >= $quantita) {
+        $sql = "SELECT * FROM " . $prefix . "aggiunta WHERE idProdotto = $idProdotto AND idCarrello = $idCarrello";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $quantitaCarrello = $row['Quantita'];
+            $quantitaCarrello += $quantita;
+            $sql = "UPDATE " . $prefix . "aggiunta SET Quantita = $quantitaCarrello WHERE idProdotto = $idProdotto AND idCarrello = $idCarrello";
             $conn->query($sql);
-            header('Location: carrello.php');
         } else {
-            echo 'Quantità non disponibile';
+            $sql = "INSERT INTO " . $prefix . "aggiunta (idProdotto, idCarrello, quantita) VALUES ($idProdotto, $idCarrello, $quantita)";
+            $conn->query($sql);
         }
+        $quantitaDisponibile -= $quantita;
+        $sql = "UPDATE " . $prefix . "prodotto SET quantita = $quantitaDisponibile WHERE ID = $idProdotto";
+        $conn->query($sql);
+        header('Location: carrello.php');
     } else {
-        echo 'Prodotto non trovato';
+        header('Location: index.php?error=quantitaNonDisponibile');
     }
 } else {
-    // echo 'Carrello non trovato';
-    $sql = "INSERT INTO carrello (idUtente) VALUES ($idUser)";
-    $conn->query($sql);
-    header('Location: addProdotto.php');
+    header('Location: index.php?error=prodottoNonTrovato');
 }
 ?>
